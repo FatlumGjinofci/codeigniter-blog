f8372a4c94002bb4ec387c01146eb3654402662f
Fatlum <fgjinofci@gmail.com>
2021-03-16 15:23:07 +0100

    added create and delete post

---
+++ application/views/posts/create.php
@@ -0,0 +1,16 @@
+<h2><?= $title ?></h2>
+
+<?php echo validation_errors(); ?>
+
+<?php echo form_open('posts/create') ?>
+  <div class="mb-3">
+    <label class="form-label">Title</label>
+    <input type="text" class="form-control" name="title" placeholder="Add Title">
+  </div>
+  <div class="mb-3">
+    <label class="form-label">Body</label>
+    <textarea name="body" class="form-control" placeholder="Add Body">
+    </textarea>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -7,3 +7,7 @@
         <?php echo $post['body'] ?>
     </p>
 </div>
+<hr>
+<?php echo form_open('/posts/delete/'. $post['id']); ?>
+    <input type="submit" value="Delete" class="btn btn-danger">
+</form>
