ce7e732ec1a99fd529cc06230868b840321d930c
Fatlum <fgjinofci@gmail.com>
2021-03-17 14:39:10 +0100

    added create comment and show comments to a post

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -15,3 +15,39 @@
 <?php echo form_open('/posts/delete/'. $post['id']); ?>
     <input type="submit" value="Delete" class="btn btn-danger">
 </form>
+<h3>Comments</h3>
+    <?php if($comments): ?>
+        <?php foreach($comments as $comment): ?>
+            <div class="well"> 
+                <h6><?php echo $comment['body']; ?> 
+                    [by <strong><?php echo $comment['name']; ?></strong>]
+                </h6>
+            </div>
+            <hr>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <p>No Comments</p>
+    <?php endif; ?>
+<h3>Add Comment</h3>
+
+<?php echo validation_errors(); ?>
+
+<?php echo form_open('comments/create/'.$post['id']); ?>
+    <div class="form-group">
+        <label for="">Name</label>
+        <input type="text" name="name" class="form-control" id="">
+    </div>
+    <div class="form-group">
+        <label for="">Email</label>
+        <input type="text" name="email" class="form-control" id="">
+    </div>
+    <div class="form-group">
+        <label for="">Body</label>
+        <textarea type="text" name="body" class="form-control" id="">
+        </textarea>
+    </div>
+    <input type="hidden" name="slug" value="<?php echo $post['slug']; ?>">
+    <button type="submit" class="btn btn-primary">
+        Submit
+    </button>
+</form>
